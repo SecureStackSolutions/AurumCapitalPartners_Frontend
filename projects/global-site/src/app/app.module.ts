b43e7c093fe22a222f334ce20b1a7f8329f1801a
Lucas <kleijvander@gmail.com>
2023-02-26 14:40:40 +0100

    feat: removes ssg and ssr capabilities

--- projects/global-site/src/app/app.module.ts
+++ projects/global-site/src/app/app.module.ts
@@ -3,15 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,
-    ScullyLibModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
