f829edf5817749315c826def609ce543ca166524
Lucas <kleijvander@gmail.com>
2023-02-26 20:52:35 +0100

    feat: sets up nx workspace with portal app

--- projects/global-site/todocker.sh
+++
@@ -1,2 +0,0 @@
-docker build -f dockerfiles/Dockerfile.spa.prod -t securestacksolutions/aurumcapitalpartners_globalsite --platform linux/amd64 ../../
-docker push securestacksolutions/aurumcapitalpartners_globalsite
--- projects/portal/todocker.sh
+++
@@ -1,2 +0,0 @@
-docker build -f dockerfiles/Dockerfile.prod -t securestacksolutions/aurumcapitalpartners_portal --platform linux/amd64 ../../
-docker push securestacksolutions/aurumcapitalpartners_portal
